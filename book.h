ea606b38012ca78c3bfb950b5db00689bfc47d64
Tyson Trương <truongniel0912@gmail.com>
2026-04-29 04:26:40 +0700

    Add book.h header file with function declarations

---
+++ book.h
@@ -0,0 +1,21 @@
+#ifndef SACH_H
+#define SACH_H
+
+#include "common.h"
+
+/* ===== KHAI BAO HAM GIAO DIEN ===== */
+
+void inDuongNgang(int n);
+void inDuongDon(int n);
+void inTieuDe(const char *tieuDe);
+void inTieuDeApp(void);
+void inThongBao(const char *msg, int loai);
+void nhanPhimTiepTuc(void);
+
+/* ===== KHAI BAO HAM SACH ===== */
+
+void themSach(int n);
+void hienThiSach(void);
+void timTheoID(void);
+
+#endif /* SACH_H */
