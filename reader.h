aa1ad82e79e3affe11ee441c0712aa100ae166b6
Tyson Trương <truongniel0912@gmail.com>
2026-04-29 08:52:50 +0700

    Update and rename reader_borrow.h to reader.h

---
+++ reader.h
@@ -0,0 +1,21 @@
+#ifndef READER_H
+#define READER_H
+
+#include "common.h"
+
+/* ===== PROTOTYPE HAM READER ===== */
+
+void        initReaderList    (ListReader *l);
+void        insertReader      (ListReader *l, Reader r);
+NodeReader *findReader        (ListReader l, char id[]);
+
+void        themReader        (ListReader *l);
+void        timReader         (ListReader l);
+void        xoaReader         (ListReader *l);
+void        showReaderList    (ListReader l);
+
+void        writeReaderToFile (ListReader l,  char filename[]);
+void        readReaderFromFile(ListReader *l, char filename[]);
+void        luuVaDoc          (ListReader *l);
+
+#endif /* READER_H */
